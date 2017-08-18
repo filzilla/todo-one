6bdfd57689ab196b4a96e77eebe80aa263ebdb56
filzilla <phillipdelia@Phillips-MacBook-Pro.local>
2017-08-18 10:48:30 -0400

    final changes

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
 
-
-
 var application = express();
 
 application.engine('mustache', mustacheExpress());
@@ -36,61 +34,32 @@ application.post('/', (request, response) => {
 
     var newTodo = request.body.newTodo;
     
-    //request.checkBody('newTodo', 'What the hell! Read the directions please.').notEmpty();
- 
-   // var errors = request.validationResult();
-    //if (errors) {
-
-      //response.redirect('todo',{todoList: todoList, errors});
-
-    //{ else {
+    
         var Todos = {};
         Todos.listItem = newTodo;
         // Todos.id =todoList.length;
         Todos.complete = false;
         Todos.id=todoList.length;
-      todoList.push(Todos);  // datafile.todo.push(newTodo)
+      todoList.push(Todos);  
       console.log(Todos);
       console.log(todoList);
      response.render('todo',{todoList:todoList});
- //   }
+
 });
 
-// application.post('/:id', (request, response)=> {
-   
-//    var item = request.params.id;
-   
 
-//    for (let i = 0; i < todoList.length; i++) {
-//        if(item === [i]){
-//        //todoList[i].complete = true;
-//        console.log(todoList[i]);
-//        console.log([i].todoList)
-//     //   [i].Todos[complete] = true};
-//        console.log(todoList[i].complete);
-       
-//        response.redirect('/');
-//    }
-//     // response.render('todo',{todoList:todoList});
-// };
 
 application.post('/:id', (request,response) =>{
 
     var item = parseInt(request.params.id);
     var todoToFind = todoList.find(q=> q.id === item);
-   // for(let i = 0; i < todoList.length; i++) {
-
-  //      if(item === todoList[i]){
+   
            
-
-       
         console.log(todoToFind.id);
         todoToFind.complete = true;
         console.log('---------------------------');
         response.render('todo',{todoList:todoList});
 
-          
-    
 });
 
 
